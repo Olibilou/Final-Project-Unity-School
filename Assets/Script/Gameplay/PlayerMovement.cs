a9b37ed6312d8f4543e312fdb88ea68f0aca4c0e
Sayepes <sayepes01@gmail.com>
2022-04-28 01:29:23 -0400

    Added mause follow system, shooting system

--- Assets/Script/Gameplay/PlayerMovement.cs
+++ Assets/Script/Gameplay/PlayerMovement.cs
@@ -10,7 +10,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private Rigidbody2D rb;
 
+
     Vector2 movement;
+    Vector2 mousePos;
+
+    public Camera cam;
 
     // Start is called before the first frame update
     void Start()
@@ -23,10 +27,16 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
