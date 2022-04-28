1ee9a7edeaecb0a84c7cc9007cf27ff9a2d35099
Sayepes <sayepes01@gmail.com>
2022-04-28 09:36:53 -0400

    update camera shoot

---
+++ Assets/Script/Gameplay/Bullet.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject hitAnimation;
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject animation = Instantiate(hitAnimation, transform.position, Quaternion.identity);
+        Destroy(animation, 5f);
+        Destroy(gameObject);
+    }
+
+}
